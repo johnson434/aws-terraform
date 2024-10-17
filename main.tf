3d0e4072d191a727e46d70abf611917e9c313925
johnson434 <ghkddlsrb96@gmail.com>
2024-10-17 09:30:10 +0900

    모듈 output 변수 참조 방식 에러
    모듈의 output 변수 참조는 경로를 지정하는 게 아닌 module.모듈명.output변수이름

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ module "ec2" {
     source = "./module/ec2"
     ami = "ami-0031d539e05f0ee86"
     tier = "t2.micro"
-    public_subnet_id = "./module/network/public_subnet_id"
+    public_subnet_id = module.network.public_subnet_id
     availability_zone = ["ap-northeast-2a", "ap-northeast-2c"]
 }
 
